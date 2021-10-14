abed8072487f3f6ea96117cc8b10613ae910be17
Jackz <me@jackz.me>
2021-10-14 12:10:01 -0500

    Add events to unified data.json

---
+++ src/js/data.js
@@ -0,0 +1,18 @@
+import data from '@/assets/data.json'
+
+export function getEvents() {
+  return data.events
+    .map(event => {
+      return {
+        ...event,
+        expanded: false
+      }
+    })
+}
+
+export function getData() {
+  return {
+    majors: data.majors,
+    places: data.places
+  }
+}
